45364a2f7c70ca6976b6abb1b1ca1b120f5ddc6c
egoing <>
2020-06-20 19:18:29 +0900

    2. 인터페이스의 형식

--- src/InterfaceApp.java
+++ src/InterfaceApp.java
@@ -1,19 +1,23 @@
 interface Calculable{
+    double PI = 3.14;
     int sum(int v1, int v2);
 }
-class RealCal implements Calculable{
+interface Printable{
+    void print();
+}
+class RealCal implements Calculable, Printable{
     public int sum(int v1, int v2) {
         return v1+v2;
     }
-}
-class DummyCal implements Calculable{
-    public int sum(int v1, int v2){
-        return 3;
+    public void print() {
+        System.out.println("This is RealCal!!");
     }
 }
 public class InterfaceApp {
     public static void main(String[] args) {
         RealCal c = new RealCal();
         System.out.println(c.sum(2,1));
+        c.print();
+        System.out.println(c.PI);
     }
 }
